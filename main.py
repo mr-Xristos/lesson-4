7de704f23c7642249993673526df43b77534ac52
mr-Xristos <egrik911@gmail.com>
2022-03-05 13:25:06 +0300

    появляется текстом но с добавлением рублей и копеек)

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-#translate = {1 : 'один', 2 : 'два', 3 : 'три', 4 : 'четыре', 5 : 'пять'}
+from number_to_string import get_string_by_number
 
 first_number = input('Введите первое число')
 operation = input('ВВедите операцию')
 second_number = input('Введите второе число')
+
+#operation = get_string_by_number(operation)
+
 if operation == '+':
-    print(int(first_number) + int(second_number))
+    print(get_string_by_number(int(first_number) + int(second_number)))
 elif operation == '-':
-    print(int(first_number) - int(second_number))
+    print(get_string_by_number(int(first_number) - int(second_number)))
 elif operation == '/':
-    print(int(first_number) / int(second_number))
+    print(get_string_by_number(int(first_number) / int(second_number)))
 elif operation == '*':
-    print(int(first_number) * int(second_number))
+    print(get_string_by_number(int(first_number) * int(second_number)))
 elif operation == '**':
-    print(int(first_number) ** int(second_number))
+    print(get_string_by_number(int(first_number) ** int(second_number)))
+else:
+    print('Вы ввели некоректное значение операции')
+
+
