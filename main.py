8732462ce4a96497e8fb5caa3a013b21e945f0f0
mr-Xristos <egrik911@gmail.com>
2022-03-13 21:36:07 +0300

    доделано

--- main.py
+++ main.py
@@ -1,22 +1,55 @@
-from number_to_string import get_string_by_number
+from num2words import num2words
 
-first_number = input('Введите первое число')
-operation = input('ВВедите операцию')
-second_number = input('Введите второе число')
+number_operation = 'y'
+number = 0
 
-#operation = get_string_by_number(operation)
+while number_operation == 'y':
+    number +=1
 
-if operation == '+':
-    print(get_string_by_number(int(first_number) + int(second_number)))
-elif operation == '-':
-    print(get_string_by_number(int(first_number) - int(second_number)))
-elif operation == '/':
-    print(get_string_by_number(int(first_number) / int(second_number)))
-elif operation == '*':
-    print(get_string_by_number(int(first_number) * int(second_number)))
-elif operation == '**':
-    print(get_string_by_number(int(first_number) ** int(second_number)))
-else:
-    print('Вы ввели некоректное значение операции')
+    try:
+        first_number = float(input("Введите первое число цифрой: "))
+    except:
+        print("нужно вводить цифру")
+
+    operation = input('ВВедите операцию')
+
+    try:
+        second_number = float(input("Введите второе число цифрой: "))
+    except:
+        print("нужно вводить цифру")
+
+
+
+    if operation == '+':
+        print(num2words(float(first_number) + float(second_number)))
+    elif operation == '-':
+        print(num2words(float(first_number) - float(second_number)))
+    elif operation == '/':
+        if second_number == 0:
+            print('не дели на ноль')
+        else:
+            print(num2words(float(first_number) / float(second_number)))
+    elif operation == '*':
+        print(num2words(float(first_number) * float(second_number)))
+    elif operation == '**':
+        print(num2words(float(first_number) ** float(second_number)))
+    else:
+        print('Вы ввели некоректное значение операции')
+
+    print(" Количество совершенных операций : " + str(number))
+    chois = None;
+    while chois != "yes" or "no":
+        chois = input(" Желаете продолжить? (yes or no) \n : ")
+        if chois == "yes":
+            break
+        elif chois == "no":
+            print(" спасибо что выбрали нас ")
+            break
+        else:
+            print(" введите yes или no ")
+    if chois == "yes":
+        continue
+    if chois == "no":
+        break
 
 
